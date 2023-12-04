1cc6d10c4f0a5df5c3b202109af6828b462fbfe2
Ian Lambert <lambe347@umn.edu>
2023-12-04 12:49:42 -0600

    solution for day 4 part 1

--- advent-2.2/Advent2-2/obj/Debug/net8.0/Advent2-2.AssemblyInfo.cs
+++ advent-2.2/Advent2-2/obj/Debug/net8.0/Advent2-2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Advent2-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c20a5b2db5675d995c7559ede7ca129aadfbcc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Advent2-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Advent2-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ advent-4.1/Advent4-1/Program.cs
@@ -0,0 +1,40 @@
+﻿var inputFile = File.ReadAllLines("input.txt");
+var inputList = new List<string>(inputFile);
+
+int total = 0;
+foreach(string line in inputList){
+    int matchCount = 0;
+
+    List<string> gameNumbers = line.Split("|", StringSplitOptions.TrimEntries).ToList()[1].Split(" ", StringSplitOptions.TrimEntries).Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
+    List<string> winningNumbers = line.Split("|", StringSplitOptions.TrimEntries).ToList()[0].Split(":", StringSplitOptions.TrimEntries).ToList()[1].Split(" ", StringSplitOptions.TrimEntries).Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
+
+    foreach(string number in gameNumbers)
+    {
+        foreach(string winningNumber in winningNumbers)
+        {
+            if(number.Equals(winningNumber) && (!number.Equals("  ")))
+            {
+                matchCount++;
+            }
+        }
+    }
+    total = calculateNewTotalPerGame(total, matchCount);
+
+    
+}
+
+Console.WriteLine(total);
+
+int calculateNewTotalPerGame(int total, int matchCount) 
+{
+        if(matchCount == 1)
+        {
+            total++;
+        }
+        if(matchCount > 1)
+        {
+            total = total + Convert.ToInt32(Math.Pow(2, matchCount - 1));
+        }
+        return total;
+}
+
