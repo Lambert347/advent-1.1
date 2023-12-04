66a44490f57ba2ab2bc1a9d9394087842089a7b6
Ian Lambert <lambe347@umn.edu>
2023-12-03 18:43:22 -0600

    solution for p2 day 1

---
+++ advent-1.2/ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allDigits = new Dictionary<string, int>()
+            {
+                {"one" , 1},
+                {"two" , 2},
+                {"three" , 3},
+                {"four" , 4},
+                {"five" , 5},
+                {"six" , 6},
+                {"seven" , 7},
+                {"eight" , 8},
+                {"nine" , 9},
+            };
+
+            for(int i = 1; i < 10; i++)
+            {
+                allDigits.Add(i.ToString(), i);
+            }
+
+            int total = 0;
+        
+            var inputFile = File.ReadAllLines("input.txt");
+            var inputList = new List<string>(inputFile);
+
+                foreach(string listLine in inputList)
+                {
+                    var firstIndex = listLine.Length;
+                    var lastIndex = -1;
+                    var firstValue = 0;
+                    var lastValue = 0;
+
+                    foreach (var digit in allDigits)
+                    {
+                        var index = listLine.IndexOf(digit.Key);
+                        if (index == -1)
+                        {
+                            continue;
+                        }
+
+                        if (index < firstIndex)
+                        {
+                            firstIndex = index;
+                            firstValue = digit.Value;
+                        }
+
+                        index = listLine.LastIndexOf(digit.Key);
+
+                        if (index > lastIndex)
+                        {
+                            lastIndex = index;
+                            lastValue = digit.Value;
+                        }
+                    }
+
+                    var fullNumber = firstValue * 10 + lastValue;
+                    total += fullNumber;                
+                }
+            Console.WriteLine(total); 
+        }
+    }
+}
