e20f86666b99a89c0a5c66d6e2d26bea7de480a7
Ian Lambert <lambe347@umn.edu>
2023-12-08 10:46:02 -0600

    solution for advent day 6 part 2

---
+++ advent-6.2/Advent6-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+var inputFile = File.ReadAllLines("input.txt");
+var inputList = new List<string>(inputFile);
+
+Regex regex = new Regex(@"\s+");
+
+var time = ReplaceWhitespace(inputList[0].Split(":").ToList()[1], regex);
+var distance = ReplaceWhitespace(inputList[1].Split(":").ToList()[1], regex);
+
+long amountOfWaysToWinPerRace = 0;
+
+long distanceToGo = Convert.ToInt64(distance);
+
+long baseTimeUsed = 0;
+long maxTime = Convert.ToInt64(time); 
+
+while(baseTimeUsed < maxTime)
+{   
+    long distancePerMillisecond = baseTimeUsed;
+
+    long timeLeft = maxTime - baseTimeUsed;
+
+    if(distancePerMillisecond * timeLeft > distanceToGo)
+    {
+        amountOfWaysToWinPerRace++;
+    }
+
+    baseTimeUsed++;
+}
+
+Console.WriteLine(amountOfWaysToWinPerRace);
+static string ReplaceWhitespace(string input, Regex regex) 
+{
+    string replacement = "";
+    return regex.Replace(input, replacement);
+}
