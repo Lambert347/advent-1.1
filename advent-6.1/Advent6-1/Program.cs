473d8914cd70da8fdf144733e29dac3ffd56e0f0
Ian Lambert <lambe347@umn.edu>
2023-12-07 21:33:56 -0600

    solution for day 6 part 1

---
+++ advent-6.1/Advent6-1/Program.cs
@@ -0,0 +1,43 @@
+﻿var inputFile = File.ReadAllLines("input.txt");
+var inputList = new List<string>(inputFile);
+
+int total = 1;
+
+var timeList = inputList[0].Split(":").ToList()[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+var distanceList = inputList[1].Split(":").ToList()[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+var amountList = new List<int>();
+
+for(int i = 0; i < timeList.Count(); i++)
+{
+    int amountOfWaysToWinPerRace = 0;
+
+    int distanceToGo = int.Parse(distanceList[i]);
+
+    int baseTimeUsed = 0;
+    int maxTime = int.Parse(timeList[i]); 
+
+    while(baseTimeUsed < maxTime)
+    {
+        
+        int distancePerMillisecond = baseTimeUsed;
+
+        int timeLeft = maxTime - baseTimeUsed;
+
+        if(distancePerMillisecond * timeLeft > distanceToGo)
+        {
+            amountOfWaysToWinPerRace++;
+        }
+
+        baseTimeUsed++;
+    }
+
+    amountList.Add(amountOfWaysToWinPerRace);    
+}
+
+foreach(int number in amountList)
+{
+    total = total * number;
+}
+
+Console.WriteLine(total);
